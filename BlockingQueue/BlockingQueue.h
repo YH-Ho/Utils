845e42025a17b61ed618c7b4b42652d0fca14fad
kevin <184573944@qq.com>
2018-10-11 14:48:27 +0800

    Add a c++11 template tool class for blocking queues.

---
+++ BlockingQueue/BlockingQueue.h
@@ -0,0 +1,96 @@
+#ifndef BLOCKINGQUEUE_H
+#define BLOCKINGQUEUE_H
+
+/* Use no less than c++11 standard */
+#pragma once
+#include <iostream>
+#include <queue>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+
+using namespace std;
+
+template<typename T>
+class BlockingQueue{
+public:
+    /* Delete the copy constructor and assignment operator */
+    BlockingQueue(const BlockingQueue<T>&) = delete;
+    BlockingQueue& operator=(const BlockingQueue<T>&) = delete;
+
+    /* Constructor with parameter of queue max size */
+    explicit BlockingQueue<T>(size_t max_size = 0) {
+        if (max_size <= 0)
+            m_max_size = SIZE_MAX;
+        else
+            m_max_size = max_size;
+    }
+
+    /* Get current queue size */
+    size_t size() const{
+        lock_guard<mutex> lk(m_mutex);
+        return m_queue.size();
+    }
+
+    /* Determine whether queue is empty */
+    bool empty() const{
+        lock_guard<mutex> lk(m_mutex);
+        return m_queue.empty();
+    }
+
+    /* Push data in queue */
+    void push(T value) {
+        unique_lock<mutex> lk(m_mutex);
+        shared_ptr<T> data(make_shared<T>(move(value)));
+        m_en_cond.wait(lk, [this] { return m_queue.size() < m_max_size; });
+        m_queue.push(data);
+        m_de_cond.notify_one();
+    }
+
+    /*  Wait and pop data out queue */
+    void wait_and_pop(T& value) {
+        unique_lock<mutex> lk(m_mutex);
+        m_de_cond.wait(lk, [this] { return !m_queue.empty(); });
+        value = move(*m_queue.front());
+        m_queue.pop();
+        m_en_cond.notify_one();
+    }
+
+    bool try_pop(T& value) {
+        lock_guard<mutex> lk(m_mutex);
+        if (m_queue.empty())
+            return false;
+        value = move(*m_queue.front());
+        m_queue.pop();
+        m_en_cond.notify_one();
+        return true;
+    }
+
+    shared_ptr<T> wait_and_pop() {
+        unique_lock<mutex> lk(m_mutex);
+        m_de_cond.wait(lk, [this] { return !m_queue.empty(); });
+        shared_ptr<T> res = m_queue.front();
+        m_queue.pop();
+        m_en_cond.notify_one();
+        return res;
+    }
+
+    shared_ptr<T> try_pop() {
+        lock_guard<mutex> lk(m_mutex);
+        if (m_queue.empty())
+            return shared_ptr<T>();
+        shared_ptr<T> res = m_queue.front();
+        m_queue.pop();
+        m_en_cond.notify_one();
+        return res;
+    }
+
+private:
+    mutable mutex m_mutex;
+    size_t m_max_size;
+    queue<shared_ptr<T> > m_queue;
+    condition_variable m_de_cond;
+    condition_variable m_en_cond;
+};
+
+#endif // BLOCKINGQUEUE_H
---
+++ BlockingQueue/test/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+#include <BlockingQueue.h>
+#include <thread>
+#include <signal.h>
+
+using namespace std;
+
+static bool g_quit = false;
+
+void *thread_1(BlockingQueue<int*> &queue) {
+    int *value = nullptr;
+    int i = 0;
+
+    while (!g_quit) {
+        i++;
+        value = &i;
+        queue.push(value);
+        //cout<<"queue push: "<<*value<<" "<<"queue size: "<<queue.size()<<endl;
+    }
+
+    return nullptr;
+}
+
+void *thread_2(BlockingQueue<int*> &queue) {
+    int i = 0;
+    //int *value = nullptr;
+
+    while (!g_quit) {
+        /* for delay */
+        if(++i % 100000000 == 0) {
+            //queue.wait_and_pop(value);
+            cout<<"queue pop: "<<**queue.wait_and_pop().get()<<" "<<"queue size: "<<queue.size()<<endl;
+        }
+    }
+
+    return nullptr;
+}
+
+void sig_handle(int arg) {
+    g_quit = true;
+}
+
+int main(int argc, char**argv) {
+    signal(SIGINT, sig_handle);
+
+    BlockingQueue<int*> test_queue(1000);
+
+    thread thd_1(thread_1,  ref(test_queue));
+    thread thd_2(thread_2,  ref(test_queue));
+
+    thd_1.join();
+    thd_2.join();
+
+    cout<<"exit"<<endl;
+    return 0;
+}
